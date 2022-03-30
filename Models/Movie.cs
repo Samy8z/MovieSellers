be8d9355e61b577511f5c84f53386451ba6278fb
Samy <samy@gmail.com>
2022-03-30 09:57:19 +0200

    Add project files.

---
+++ Models/Movie.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Iut.Demo.Web.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
+        public string? Title { get; set; }
+
+        [DataType(DataType.Date)]
+        
+        [Required]
+        [StringLength(30)]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        public DateTime ReleaseDate { get; set; }
+        public string? Genre { get; set; }
+        public decimal Price { get; set; }
+
+        public string? Rating { get; set; }
+    }
+}
